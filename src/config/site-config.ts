abac0d9bf809d41437f95f0163a5e195b30f31b3
Kev_CB <kevcb369@gmail.com>
2024-10-06 11:25:55 -0500

    Antes de Collections

---
+++ src/config/site-config.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    title: "Hogwarts's Blog",
+    description: "Blog encargado de compartir la actualidad sobre lo que sucede Hogwarts durante tu año escolar y otras cosas más."
+}
